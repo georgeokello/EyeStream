93948379c892f52267d9fd1a0c654b703156c500
george okello3 <georgeokello335@gmail.com>
2022-03-05 23:29:39 +0300

    added packeages in requirements.txt

--- eyestream/models.py
+++ eyestream/models.py
@@ -9,7 +9,7 @@ from django.core.files.storage import default_storage as storage
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='media/default.jpg',null=True, blank=True, upload_to='profile_pics')
+    image = models.ImageField(default='default.jpg',null=True, blank=True, upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
